b0b6da997365e9e1a96c597d310aebabe8e9b8da
Senya Volkov <formobileprima@gmail.com>
2020-03-17 19:28:17 +0200

    Updates

---
+++ precache-manifest.f454784f146e8d52e7c9ef274cf838c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dcfc790a27f8f86a79f2b914938689",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1af729ca95cbca9bced5",
+    "url": "./static/css/main.d99e7c34.chunk.css"
+  },
+  {
+    "revision": "ff67a947fec39aeb0d38",
+    "url": "./static/js/2.77858075.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "./static/js/2.77858075.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1af729ca95cbca9bced5",
+    "url": "./static/js/main.ddd91439.chunk.js"
+  },
+  {
+    "revision": "3c48c15f9ec8cf023aae",
+    "url": "./static/js/runtime-main.37eb518b.js"
+  }
+]);
